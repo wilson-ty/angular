a5b78bdda32d69fe8534400e6ff77a0621eb0d75
wilson-ty <129957019+wilson-ty@users.noreply.github.com>
2023-05-26 06:39:30 +0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'teamapp';
+ newMemberName = "";
+ members: string[] = [];
+
+ onInput(member:string){
+  this.newMemberName = member;
+  console.log(this.newMemberName)
+ }
+
+ addMember() {
+  this.members.push(this.newMemberName);
+  this.newMemberName ="";
+  console.log(this.members);
+ }
 }
